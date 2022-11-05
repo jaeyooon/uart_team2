f7dd325902197dd4ca9b91bf878fd7310e63636a
walkwa <citypass0119@naver.com>
2022-11-05 22:29:35 +0900

    KItemService 안쓰는 코드 제거

--- uart_finalproject/src/main/java/com/multi/service/KItemService.java
+++ uart_finalproject/src/main/java/com/multi/service/KItemService.java
@@ -3,7 +3,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.multi.dto.KItemDTO;
-import com.multi.dto.WishlistDTO;
 import com.multi.frame.MyService;
 import com.multi.mapper.KItemMapper;
 @Service
