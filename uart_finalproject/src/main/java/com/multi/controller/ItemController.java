4ff216a03b0bb0001be1c37cf020255cef49334c
Heyonjee <guswl409@gmail.com>
2022-11-12 11:50:21 +0900

    전시목록 dropdown 전시시작순,마감순 기능구현

--- uart_finalproject/src/main/java/com/multi/controller/ItemController.java
+++ uart_finalproject/src/main/java/com/multi/controller/ItemController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.multi.dto.ItemDTO;
 import com.multi.dto.ReviewDTO;
+import com.multi.mapper.ItemMapper;
 import com.multi.service.ItemService;
 import com.multi.service.ReviewService;
 
@@ -19,6 +20,8 @@ public class ItemController {
 	@Autowired
 	ItemService item_service;
 
+	@Autowired
+	ItemMapper item_mapper;
 
 	@Autowired
 	ReviewService re_service;
@@ -72,6 +75,120 @@ public class ItemController {
 		List<ItemDTO> list = null;
 		try {
 			list = item_service.select_cate(4);
+			model.addAttribute("itemlist4", list);
+			model.addAttribute("center", dir+"speciallist");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "main";
+	}
+	
+	
+	@RequestMapping("/end_m")
+	public String end_m(Model model) {
+		List<ItemDTO> list = null;
+		try {
+			list = item_mapper.end_fast(1);
+			model.addAttribute("itemlist1", list);
+			model.addAttribute("center", dir+"medialist");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "main";
+	}
+	
+	@RequestMapping("/end_e")
+	public String end_e(Model model) {
+		List<ItemDTO> list = null;
+		try {		
+			list = item_mapper.end_fast(2);
+			model.addAttribute("itemlist2", list);
+			model.addAttribute("center", dir+"explist");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "main";
+	}
+	
+	@RequestMapping("/end_p")
+	public String end_p(Model model) {
+		List<ItemDTO> list = null;
+		try {		
+			list = item_mapper.end_fast(3);
+			model.addAttribute("itemlist3", list);
+			model.addAttribute("center", dir+"privatelist");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "main";
+	}
+	
+	@RequestMapping("/end_s")
+	public String end_s(Model model) {
+		List<ItemDTO> list = null;
+		try {		
+			list = item_mapper.end_fast(4);
+			model.addAttribute("itemlist4", list);
+			model.addAttribute("center", dir+"speciallist");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "main";
+	}
+	
+	
+	@RequestMapping("/start_m")
+	public String start_m(Model model) {
+		List<ItemDTO> list = null;
+		try {
+			list = item_mapper.start_fast(1);
+			model.addAttribute("itemlist1", list);
+			model.addAttribute("center", dir+"medialist");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "main";
+	}
+	
+	@RequestMapping("/start_e")
+	public String start_e(Model model) {
+		List<ItemDTO> list = null;
+		try {			
+			list = item_mapper.start_fast(2);
+			model.addAttribute("itemlist2", list);
+			model.addAttribute("center", dir+"explist");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "main";
+	}
+	
+	@RequestMapping("/start_p")
+	public String start_p(Model model) {
+		List<ItemDTO> list = null;
+		try {			
+			list = item_mapper.start_fast(3);
+			model.addAttribute("itemlist3", list);
+			model.addAttribute("center", dir+"privatelist");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "main";
+	}
+	
+	@RequestMapping("/start_s")
+	public String start_s(Model model) {
+		List<ItemDTO> list = null;
+		try {			
+			list = item_mapper.start_fast(4);
 			model.addAttribute("itemlist4", list);
 			model.addAttribute("center", dir+"speciallist");
 		} catch (Exception e) {
--- uart_finalproject/src/main/java/com/multi/mapper/ItemMapper.java
+++ uart_finalproject/src/main/java/com/multi/mapper/ItemMapper.java
@@ -12,9 +12,11 @@ import com.multi.frame.MyMapper;
 @Mapper
 public interface ItemMapper extends MyMapper<Integer, ItemDTO>{
 	public List<ItemDTO> select_cate(Integer k) throws Exception;
-	public List<ItemDTO> searchitem(String txt) throws Exception;
+	public List<ItemDTO> searchitem(String txt) throws Exception;  //Service에 안써도 돼는 이유=> 트랜잭션이 필요없기 때문, 실패하면 데이터를 조회하지 못할 뿐 DB에 영향 없음. 데이터조작이 필요없음
 	public List<ItemDTO> gethighgrade() throws Exception;
 	public <V> V select2(Integer k) throws Exception;
 	public <V> V select3(Integer k) throws Exception;
+	public List<ItemDTO> start_fast(Integer k) throws Exception;  //Service에 없음
+	public List<ItemDTO> end_fast(Integer k) throws Exception;  //Service에 없음
 
 }
