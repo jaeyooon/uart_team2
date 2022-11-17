61078d3ac2f0cf3731564b9189ea36ca8224b616
wanttobodybuilderifbbpro <wogud114@gmail.com>
2022-11-17 17:15:52 +0900

    JH has changed something.

--- uart_finalproject_Admin/src/main/java/com/multi/controller/AdminController.java
+++ uart_finalproject_Admin/src/main/java/com/multi/controller/AdminController.java
@@ -1,13 +1,20 @@
 package com.multi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.multi.dto.AdminDTO;
+import com.multi.service.AdminService;
+
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-
+	
+	@Autowired
+	AdminService service;
+	
 	String dir = "admin/";
 	
 	@RequestMapping("/get")
@@ -20,6 +27,18 @@ public class AdminController {
 	@RequestMapping("/register")
 	public String register(Model model) {
 		model.addAttribute("center",dir+"register");
+		return "main";
+	}
+	
+	@RequestMapping("/registerimpl")
+	public String registerimpl(Model model, AdminDTO adm) {
+		try {
+			service.register(adm);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return "main";
 	}
 	
