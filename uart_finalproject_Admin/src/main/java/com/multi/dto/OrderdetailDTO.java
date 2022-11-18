7ad6c0e44ad9122f174e240ef0c32b523fcced89
wanttobodybuilderifbbpro <wogud114@gmail.com>
2022-11-18 13:19:26 +0900

    Something has been modified.

---
+++ uart_finalproject_Admin/src/main/java/com/multi/dto/OrderdetailDTO.java
@@ -0,0 +1,26 @@
+package com.multi.dto;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class OrderdetailDTO {
+	private Integer orderlistid;
+	private String item_itemname;
+	private Date orderlist_orderdate;
+	private Date item_estart;
+	private Date item_efin;
+	private Integer orderlist_ordercnt;
+	private String item_itemimg;
+	private String item_place;
+	private Date orderlist_candate;
+}
