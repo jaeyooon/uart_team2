810384b5e76792e755697b8c5d1c07238fa4fd32
wanttobodybuilderifbbpro <wogud114@gmail.com>
2022-11-21 09:33:44 +0900

    JH has added OrderDetail's appearance of CRUD.

---
+++ uart_finalproject_Admin/src/main/java/com/multi/mapper/OrderdetailMapper.java
@@ -0,0 +1,14 @@
+package com.multi.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import com.multi.dto.OrderdetailDTO;
+import com.multi.frame.MyMapper;
+
+@Repository
+@Mapper
+public interface OrderdetailMapper extends MyMapper<Integer, OrderdetailDTO>{	
+}
