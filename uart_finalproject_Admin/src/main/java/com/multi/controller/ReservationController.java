eb8beeec63d896c3daedaa8ddf1bbe3b04b0b408
wanttobodybuilderifbbpro <wogud114@gmail.com>
2022-11-21 16:53:06 +0900

    1st step

--- uart_finalproject_Admin/src/main/java/com/multi/controller/ReservationController.java
+++ uart_finalproject_Admin/src/main/java/com/multi/controller/ReservationController.java
@@ -2,23 +2,34 @@ package com.multi.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.multi.dto.OrderdetailDTO;
+import com.multi.service.OrderdetailService;
 
 @Controller
 @RequestMapping("/reservation")
 public class ReservationController {
-
+	
+	@Autowired
+	OrderdetailService service;
+	
 	String dir = "reservation/";
 	
 	@RequestMapping("/get")
 	public String get(Model model) {
 		List<OrderdetailDTO> list = null;
 		
-		
+		try {
+			list=service.get();
+			model.addAttribute("list",list);
+			model.addAttribute("center",dir+"get");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		return "main";
 	}
