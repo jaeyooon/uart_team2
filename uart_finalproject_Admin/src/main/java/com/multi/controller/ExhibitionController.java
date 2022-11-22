2f64369a8ca719403ab75ef0ffb21b55e17a5eaf
Heyonjee <guswl409@gmail.com>
2022-11-22 10:59:59 +0900

    admin application 수정

--- uart_finalproject_Admin/src/main/java/com/multi/controller/ExhibitionController.java
+++ uart_finalproject_Admin/src/main/java/com/multi/controller/ExhibitionController.java
@@ -1,8 +1,16 @@
 package com.multi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.multi.dto.ItemDTO;
+import com.multi.dto.ItemoptionDTO;
+import com.multi.service.ItemService;
+import com.multi.service.ItemoptionService;
 
 @Controller
 @RequestMapping("/exhibition")
@@ -10,9 +18,25 @@ public class ExhibitionController {
 
 	String dir = "exhibition/";
 	
+	@Autowired
+	ItemService service;
+	
+	@Autowired
+	ItemoptionService option_service;
+	
+	
 	@RequestMapping("/get")
 	public String get(Model model) {
-		model.addAttribute("center",dir+"get");
+		List<ItemDTO> list= null;
+		try {
+			list=service.get();
+			model.addAttribute("list", list);
+			model.addAttribute("center",dir+"get");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return "main";
 	}
 	
@@ -24,6 +48,18 @@ public class ExhibitionController {
 	}
 	
 
+	@RequestMapping("/register_price")
+	public String register_price(Model model) {
+		model.addAttribute("center",dir+"price");
+		return "main";
+	}
+	
+	@RequestMapping("/registerimpl_price")
+	public String registerimpl_price(Model model, ItemoptionDTO option) throws Exception {
+		option_service.register(option);
+		
+		return "redirect:get";
+	}
 	
 	
 	
